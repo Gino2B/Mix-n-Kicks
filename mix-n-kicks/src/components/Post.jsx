54fb854cd3350d391865043f8253ad4e09c1ffe5
Gino2B <gbaybay30@gmail.com>
2021-06-29 14:17:39 -0400

    Added states for the post form component

--- mix-n-kicks/src/components/Post.jsx
+++ mix-n-kicks/src/components/Post.jsx
@@ -1,15 +1,51 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 function Post(props) {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("");
+  const [brand, setBrand] = useState("");
+  const [colorway, setColorway] = useState("");
+  const [size, setSize] = useState(0);
+  const [description, setDescription] = useState("");
+
   return (
     <form>
-      <input type="text" placeholder="Name"></input>
-      <input type="text" placeholder="Image URL"></input>
-      <input type="text" placeholder="Shoe Brand"></input>
-      <input type="text" placeholder="Colorway"></input>
-      <input type="number" placeholder="Size"></input>
-      <textarea placeholder="comments"></textarea>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder="Name"
+      ></input>
+      <input
+        type="text"
+        value={image}
+        onChange={(e) => setImage(e.target.value)}
+        placeholder="Image URL"
+      ></input>
+      <input
+        type="text"
+        value={brand}
+        onChange={(e) => setBrand(e.target.value)}
+        placeholder="Shoe Brand"
+      ></input>
+      <input
+        type="text"
+        value={colorway}
+        onChange={(e) => setColorway(e.target.value)}
+        placeholder="Colorway"
+      ></input>
+      <input
+        type="number"
+        value={size}
+        onChange={(e) => setSize(e.target.valueAsNumber)}
+        placeholder="Size"
+      ></input>
+      <textarea
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+        placeholder="comments"
+      ></textarea>
     </form>
   );
 }
